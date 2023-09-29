d7a3b3cad0e862c5c0d125030fbe89b71656e485
Henrique Samensari Borges <45943336+rickborges93@users.noreply.github.com>
2023-09-29 10:27:48 -0300

    Task logic and visual adjustment

--- todo/src/components/Task.tsx
+++ todo/src/components/Task.tsx
@@ -1,15 +1,23 @@
 import styles from './Task.module.css';
 import { Trash } from 'phosphor-react';
 
-export function Task() {
+interface TaskComponentProps {
+    id: string;
+    content: string;
+    isChecked: boolean;
+    handleRemoveTask: (id: string) => void;
+    handleCheckTask: (id: string) => void;
+}
+
+export function Task({ id, content, isChecked, handleRemoveTask, handleCheckTask }: TaskComponentProps) {
     return (
         <div className={ styles.taskContent }>            
             <label className={ styles.container }>                
-                Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
-                <input type='checkbox' />
+                { content }
+                <input type='checkbox' defaultChecked={isChecked} onChange={() => handleCheckTask(id)} />
                 <span className={ styles.checkmark }></span>
             </label>
-            <Trash size={16} />
+            <Trash size={16} onClick={() => handleRemoveTask(id)} />
         </div>
     )
 }
